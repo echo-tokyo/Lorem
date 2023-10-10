bb8d468d6279b4746cb6158ab837e928da231201
sayasha <2two2two8eight@mail.ru>
2023-10-11 02:50:22 +0300

    niche osobogo

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,10 @@ const mySlider = new Splide('#mySlider', {
     perPage: 4,
     gap: '10px',
     pagination: false,
+    breakpoints: {
+        1900: {
+            perPage: 3,
+        }
+    }
 }) 
 mySlider.mount()
